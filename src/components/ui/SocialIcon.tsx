5542b5bd19aa8bcd979889a1225b1cc107bd8220
hemantyadav8006 <hemant.kumar@kollegeapply.com>
2025-04-30 20:21:37 +0530

    Add react-hook-form for form handling in Login component; update SocialIcon to accept URL prop

--- src/components/ui/SocialIcon.tsx
+++ src/components/ui/SocialIcon.tsx
@@ -1,6 +1,12 @@
-export const SocialIcon = ({ Icon }: { Icon: React.ElementType }) => (
+export const SocialIcon = ({
+  Icon,
+  url,
+}: {
+  Icon: React.ElementType;
+  url?: string;
+}) => (
   <a
-    href="#"
+    href={url}
     className="border border-gray-300 rounded-full w-10 h-10 flex items-center justify-center text-gray-600 hover:bg-gray-200 transition"
   >
     <Icon />
